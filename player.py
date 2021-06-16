ceed4a11be2df9581e8c165de9c8481890e3d0ca
CyrusWarner <cyruswarner27@gmail.com>
2021-06-16 14:47:29 -0500

    Added user validation if gesture is misspelled.

--- player.py
+++ player.py
@@ -6,14 +6,17 @@ class Player:
         self.gesture_list = ["Rock", "Scissors", "Paper", "Lizard", "Spock"]
 
     def choose_player_gesture(self):
-        gesture_input = input(f"{self.name} Choose your gesture!")
+        gesture_input = input(f"\n{self.name} Choose your gesture!")
         for gesture in self.gesture_list:
             if gesture_input.lower() == gesture.lower():
                 self.chosen_gesture = gesture
+                return
+        else:
+            print("invalid input\n")
+            self.choose_player_gesture()
 
     def show_gesture_options(self):
         gesture_index = 0
         for each in self.gesture_list:
             print(each)
             gesture_index += 1
-
